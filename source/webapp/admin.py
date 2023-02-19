0443466f3935ef9b833a595760354ca210bb037c
Nurmuhamed Bekinulu <nurmuhamedbekinulu@gmail.com>
2023-02-19 10:41:56 +0600

    Добавления моделей категорий и товаров

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from webapp.models import Article
 
 
 # Register your models here.
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -2,3 +2,29 @@ from django.db import models
 
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=200, null=False, blank=False, verbose_name="Наименование")
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name="Описание")
+
+    def __str__(self):
+        return f"{self.category_name} - {self.category_name}"
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200, null=False, blank=False, verbose_name="Наименование")
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name="Описание")
+    category = models.ForeignKey(
+        to='webapp.Category',
+        verbose_name='Категория',
+        null=False,
+        blank=False,
+        related_name='categories',
+        on_delete=models.RESTRICT
+        )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время и дата создания")
+    price = models.DecimalField(decimal_places=2, max_digits=20, null=False, blank=False, verbose_name="Цена")
+    image_url = models.CharField(max_length=200, null=False, blank=False, verbose_name="Картинка")
+
+
+    def __str__(self):
+        return f"{self.text} - {self.created_at}"
