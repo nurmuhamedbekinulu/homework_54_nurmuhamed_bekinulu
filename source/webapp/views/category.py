22577d76083b3e337f0d65a5a855df28e2e691ae
Nurmuhamed Bekinulu <nurmuhamedbekinulu@gmail.com>
2023-02-19 10:57:55 +0600

    view для отображения деталей категорий

--- source/webapp/views/category.py
+++ source/webapp/views/category.py
@@ -1,2 +1,24 @@
-class Category():
-    pass
+from django.core.handlers.wsgi import WSGIRequest
+from django.shortcuts import render, redirect, get_object_or_404
+from webapp.models import Category
+from django.http import HttpResponseNotFound
+from django.urls import reverse
+
+
+# def add_view(request: WSGIRequest):
+#     if request.method == "GET":
+#         return render(request, 'article_create.html')
+#     article_data = {
+#         'title': request.POST.get('title'),
+#         'text': request.POST.get('text'),
+#         'author': request.POST.get('author')
+#     }
+#     article = Article.objects.create(**article_data)
+#     return redirect('article_detail', pk=article.pk)
+
+
+def detail_view(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    return render(request, 'category.html', context={
+        'category': category
+        })
